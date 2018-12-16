ad044fff8ead33da8bf49fbeccdf74dc9d69cfcb
xenleme <xenleme@gmail.com>
2018-12-16 17:25:52 +0300

    Add highlighting for remove output button

--- public/js/subject.js
+++ public/js/subject.js
@@ -1,11 +1,14 @@
 const subjectOutput = document.getElementById('subjectOutput');
+const removeOutputBtn = document.getElementById('remove-output-btn');
 
 function addSubject() {
   let subjectInput = document.getElementById('subjectInput');
   subjectOutput.innerHTML = subjectInput.value;
   subjectInput.value = '';
+  removeOutputBtn.className = 'subject-btn remove-output';
 }
 
 function removeOutput() {
   subjectOutput.innerHTML = '';
+  removeOutputBtn.className = 'subject-btn';
 }
